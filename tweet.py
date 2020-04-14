19b50f1c658f8354a7e53c46b5a75e7b99975f3d
Retsuki <s1f101701845@iniad.org>
2020-04-14 10:24:28 +0900

    ogpの変更で画像を投稿させなくて良くなった

--- tweet.py
+++ tweet.py
@@ -14,21 +14,10 @@ ATS = os.environ["ATS"]
 twitter = OAuth1Session(CK, CS, AT, ATS)
 
 url = "https://api.twitter.com/1.1/statuses/update.json"
-url_media = "https://upload.twitter.com/1.1/media/upload.json"
-img_url = "https://ishikawacorona.s3-ap-northeast-1.amazonaws.com/readmeishikawa.png"
 
-response = urllib.request.urlopen(img_url)
-data = response.read()
-files = {"media": data}
-req_media = twitter.post(url_media, files=files)
-
-# media_id を取得
-media_id = json.loads(req_media.text)['media_id']
-
-tweet = "おはよんよん！！\n本日の石川県の新型コロナウイルス感染者の状況です。\n https://ishikawa-covid19.netlify.com/"
+tweet = "おはよんよん！！\n本日の石川県の新型コロナウイルス感染者の状況です。\n https://covid19-ishikawa.netlify.com/ \n #石川県\n新型コロナウイルス"
 params = {
     "status": tweet,
-    "media_ids": [media_id]
 }
 res = twitter.post(url, params=params)
 
