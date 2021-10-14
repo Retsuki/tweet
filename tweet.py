3b3afd2cad0b29378e0c1eeaa6cf944a45b5e523
Retsuki <s1f101701845@iniad.org>
2021-10-14 16:41:05 +0900

    UPDATE: text

--- tweet.py
+++ tweet.py
@@ -5,6 +5,7 @@ import sys
 import os
 import datetime
 import json
+import datetime
 
 CK = os.environ.get("CK")
 CS = os.environ["CS"]
@@ -15,9 +16,14 @@ twitter = OAuth1Session(CK, CS, AT, ATS)
 
 url = "https://api.twitter.com/1.1/statuses/update.json"
 
-tweet = "おはよんよん！！"
+tweet_list = [
+    "応募待ってます！ by Scheeme株式会社\n#wantedly\nhttps://www.wantedly.com/projects/754839?utm_source=t.co&utm_medium=share&lang=en ",
+    "応募待ってます！ by Scheeme株式会社\n#wantedly\nhttps://www.wantedly.com/projects/754835?utm_source=t.co&utm_medium=share&lang=en "
+]
+
+dt_now = datetime.datetime.now()
 params = {
-    "status": tweet,
+    "status": tweet_list[0] if dt_now.day % 2 == 0 else tweet_list[1]
 }
 res = twitter.post(url, params=params)
 
